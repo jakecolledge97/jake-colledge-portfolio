6b801b6b6f581f33d1f2ff39bfc1c179d12bde75
John Conway <88188872+jakecolledge97@users.noreply.github.com>
2021-10-27 22:38:17 +1030

    added button function in new.html that will create a header

--- landing.js
+++ landing.js
@@ -1,3 +1,21 @@
-$('.whoBtn').click(function(){
-    location.href = "index.html"
+
+$('.whoBtn').click(
+    function(){
+        $('.btnContainer').animate({
+            top: '0px'
+    },1000)
+    $('.btnContainer').css({'top': '0','width': '191px'})
+    $('.btnContainer').animate({
+        width: '100%',
+        borderRadius: '0px',
+        margin: '0px'
+    },1000, function(){
+        $('.whoBtn').fadeOut('slow',function(){
+            $(this).css('display','none')
+            $('.hamburger').fadeIn('slow')
+        })
+        $('.btnContainer').css('height','56px')
+        
+    })
+    
 })
