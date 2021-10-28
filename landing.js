baeb4f599349a6de3ab57e76a78596d6d15748e3
Jake Colledge <jakecolledgework@gmail.com>
2021-10-29 01:45:37 +1030

    added containers for about me info

--- landing.js
+++ landing.js
@@ -1,3 +1,4 @@
+let header = "This is Jake Colledge."
 
 $('.whoBtn').click(
     function(){
@@ -10,12 +11,25 @@ $('.whoBtn').click(
         borderRadius: '0px',
         margin: '0px'
     },1000, function(){
-        $('.whoBtn').fadeOut('slow',function(){
-            $(this).css('display','none')
-            $('.hamburger').fadeIn('slow')
+        $('.whoBtn').fadeOut('fast',function(){
+            //$(this).css('display','none')
+            $('.hamburger').fadeIn('fast')
         })
         $('.btnContainer').css({'height':'56px','position': 'fixed'})
-        
+        $('body').css('overflow-y','scroll')
+        $('.me').animate({
+            height: '100%'
+        },500)
+        $('.containers').fadeIn('fast', function(){
+            $(this).animate({
+                height: '25vh',
+                padding: '5px'
+            },500)
+            $('.info-container').animate({
+                marginTop: '56px'
+            },500)
+        })
+
     })
     
 })
